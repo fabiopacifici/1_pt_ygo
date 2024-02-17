243ac81c7b1ec665a4d3990c59d4bb24b1b85f9d
fbhood <mail@fabiopacifici.com>
2024-02-17 10:48:46 +0100

    add archetypes filter

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,14 @@
 <script>
-import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 export default {
   name: 'App',
-  data() {
-    return {
-      cards: [],
-    };
-  },
-  mounted() {
-    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
-      .then(response => {
-        console.log(response.data.data);
-        this.cards = response.data.data;
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  },
   components: { AppHeader, AppMain }
 }
 </script>
 <template>
   <AppHeader></AppHeader>
   <AppMain></AppMain>
-
-  <div>
-
-    <div class="card" v-for="card in cards">
-      <img :src="card.card_images[0].image_url">
-      <h3>{{ card.name }}</h3>
-      <div>
-        {{ card.archetype }}
-      </div>
-    </div>
-
-  </div>
 </template>
 
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,89 @@
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'AppMain'
+  name: 'AppMain',
+  data() {
+    return {
+      cards: [],
+      archetypes: [],
+      selectedArchetype: '',
+    };
+  },
+  methods: {
+    filterResults() {
+      console.log('filter all cards');
+      const url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0&archetype=${this.selectedArchetype}`
+
+      console.log(url);
+      axios.get(url)
+        .then(response => {
+          console.log(response.data.data);
+          this.cards = response.data.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+    }
+  },
+  mounted() {
+
+    /* Get all archetypes */
+    axios.get('https://db.ygoprodeck.com/api/v7/archetypes.php')
+      .then(response => {
+        console.log(response.data);
+        this.archetypes = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+
+    /* Get all Cards */
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+      .then(response => {
+        console.log(response.data.data);
+        this.cards = response.data.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  },
 }
 </script>
 <template>
   <main>
     MAIN Content
+
+    <!-- filter  -->
+    <select name="archetype" id="archetype" v-model="selectedArchetype" @change="filterResults">
+      <option value="">All</option>
+      <!-- all archetypes here -->
+      <option :value="archetype.archetype_name" v-for="archetype in archetypes">
+        {{ archetype.archetype_name }}
+      </option>
+    </select>
+
+    <!-- total results -->
+
+    <!-- cards list -->
+
+    <section class="cards">
+      <div class="container">
+        <div class="row">
+          <div class="col" v-for="card in cards">
+            <div class="card">
+              <img :src="card.card_images[0].image_url">
+              <h3>{{ card.name }}</h3>
+              <div>
+                {{ card.archetype }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
   </main>
 </template>
 
