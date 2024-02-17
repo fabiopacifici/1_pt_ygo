bd75e8144501f134023950368e03d888cd7efdfd
fbhood <mail@fabiopacifici.com>
2024-02-17 11:39:21 +0100

    add card item component

---
+++ src/components/CardItem.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'CardItem',
+  props: {
+    card: Object
+  }
+}
+</script>
+
+<template>
+  <div class="card">
+    <img :src="card.card_images[0].image_url">
+    <h3>{{ card.name }}</h3>
+    <div>
+      {{ card.archetype }}
+    </div>
+  </div>
+</template>
+
+
+<style scoped></style>
