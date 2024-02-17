e307f84b06ed53137607cfc56f150908aaee954a
fbhood <mail@fabiopacifici.com>
2024-02-17 10:04:10 +0100

    add global state file

---
+++ src/state.js
@@ -0,0 +1,7 @@
+import {reactive} from 'vue'
+
+export const state = reactive({
+  /* Reactive object - global state */
+  api_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+  
+})
